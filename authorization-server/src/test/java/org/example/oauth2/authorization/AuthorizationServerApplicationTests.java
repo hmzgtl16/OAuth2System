7eaebefba57249418cc9d92c65d55424913b0e1f
hmzgt <hmzgtl16@gmail.com>
2025-07-19 18:33:56 +0100

    Initialize Authorization Server, Client Server, and Resource Server projects with basic Spring Boot configurations, Gradle build scripts, and Git settings.

---
+++ authorization-server/src/test/java/org/example/oauth2/authorization/AuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.oauth2.authorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
